4f5ea19dcd0a199bd1c9e7d3ad445347fca7ac5e
Jianbo Liu <jianbol@andrew.cmu.edu>
2019-03-08 22:04:27 -0800

    replace time.sleep with conditional wait in lc_login

--- leetcode-autologin/lc_login.py
+++ leetcode-autologin/lc_login.py
@@ -7,41 +7,51 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 
-def login(url, username, password, DRIVER_PATH):
+def get_driver(DRIVER_PATH):
     options = webdriver.ChromeOptions()
     options.add_argument('--disable-extensions')
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(DRIVER_PATH, chrome_options=options)
+    return webdriver.Chrome(DRIVER_PATH, chrome_options=options)
+
+def login(url, username, password, driver):
     driver.get(url)
-    time.sleep(10)
+    element = WebDriverWait(driver, 10).until(
+        EC.invisibility_of_element_located((By.ID, "initial-loading"))
+    )
+    driver.find_element_by_id("username-input").send_keys(username)
+    driver.find_element_by_id("password-input").send_keys(password)
+    driver.find_element_by_id("sign-in-button").click()
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "nav-user-app"))
+    )
 
-    try:
-        driver.find_element_by_id("username-input").send_keys(username)
-        driver.find_element_by_id("password-input").send_keys(password)
-        driver.find_element_by_id("sign-in-button").click()
-
-        element = WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.ID, "user-progress-app"))
-        )
-        progress = driver.find_element_by_css_selector("#user-progress-app div.progress-status div:nth-of-type(2)")
-        print(progress.find_element_by_tag_name('div').get_attribute('innerHTML'))
-    finally:
-        driver.quit()
-
+def get_progress(url, driver):
+    driver.get(url)
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "#user-progress-app div.progress-status"))
+    )
+    
+    progress = driver.find_element_by_css_selector("#user-progress-app div.progress-status div.text-success div.status")
+    print(progress.get_attribute('innerHTML'))
 
 def main():
     if len(sys.argv) < 3:
         print('Usage: python3 lc_login.py <username> <password> <driver-path>')
         return
     url = 'https://leetcode.com/accounts/login/'
+    url_problems = 'https://leetcode.com/problemset/all/'
     username = sys.argv[1]
     password = sys.argv[2]
     DRIVER_PATH = sys.argv[3]
     
-    login(url, username, password, DRIVER_PATH)
-
+    try:
+        driver = get_driver(DRIVER_PATH)
+        login(url, username, password, driver)
+        get_progress(url_problems, driver)
+    finally:
+        driver.quit()
 
 if __name__ == "__main__":
     main()
