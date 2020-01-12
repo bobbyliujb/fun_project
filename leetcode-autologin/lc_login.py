5cce1818306c1f3c44708422e1afae9c37301d03
Jianbo Liu <jianbol@andrew.cmu.edu>
2020-01-12 12:28:48 -0800

    enhance auto login and get progress; added sign off

--- leetcode-autologin/lc_login.py
+++ leetcode-autologin/lc_login.py
@@ -9,39 +9,70 @@ from selenium.webdriver.common.by import By
 
 def get_driver(DRIVER_PATH):
     options = webdriver.ChromeOptions()
-    options.add_argument('--disable-extensions')
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
+    options.add_argument("--disable-extensions")
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
     return webdriver.Chrome(DRIVER_PATH, chrome_options=options)
 
-def login(url, username, password, driver):
-    driver.get(url)
-    element = WebDriverWait(driver, 10).until(
-        EC.invisibility_of_element_located((By.ID, "initial-loading"))
-    )
-    driver.find_element_by_css_selector("input[data-cy='username']").send_keys(username)
-    driver.find_element_by_css_selector("input[data-cy='password']").send_keys(password)
-    driver.find_element_by_css_selector("button[data-cy='sign-in-btn']").click()
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "nav-user-app"))
-    )
+"""
+Login using given credentials. Print the page if sign in takes a long time
+"""
+def login(url, username, password, driver, max_retry = 3):
+    retry_count = 0
+    while retry_count < max_retry:
+        driver.get(url)
+        element = WebDriverWait(driver, 10).until(
+            EC.invisibility_of_element_located((By.ID, "initial-loading"))
+        )
+        driver.find_element_by_css_selector("input[data-cy='username']").send_keys(username)
+        driver.find_element_by_css_selector("input[data-cy='password']").send_keys(password)
+        driver.find_element_by_css_selector("button[data-cy='sign-in-btn']").click()
+        try:
+            element = WebDriverWait(driver, 15).until(
+                EC.presence_of_element_located((By.ID, "nav-user-app"))
+            )
+            return
+        except TimeoutException:
+            retry_count += 1
+    print(driver.find_element_by_css_selector("body div").text)
 
+"""
+Print status from progress page
+"""
 def get_progress(url, driver):
     driver.get(url)
     element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "#user-progress-app div.progress-status"))
+        EC.presence_of_element_located((By.ID, "ac_total"))
     )
-    
-    progress = driver.find_element_by_css_selector("#user-progress-app div.progress-status div.text-success div.status")
-    print(progress.get_attribute('innerHTML'))
+    current_accepted = 0
+    while True:     # The number will increment to final stage so we need to compare until it does not change
+        accepted = int(driver.find_element_by_id("ac_total").get_attribute("innerHTML"))
+        if accepted == current_accepted:
+            break
+        else:
+            current_accepted = accepted
+            time.sleep(0.5)
+    total = driver.find_element_by_css_selector("#ac_total + span")
+    print("%d %s" % (current_accepted, total.get_attribute("innerHTML")))
+
+"""
+Sign off
+"""
+def sign_out(url, driver):
+    driver.get(url)
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "nav-user-app"))
+    )
+    driver.find_element_by_id("nav-user-app").click()
+    driver.find_element_by_css_selector("#nav-user-app span ul .dropdown-view .option-list .list-item:nth-child(3)").click()
 
 def main():
     if len(sys.argv) < 3:
-        print('Usage: python3 lc_login.py <username> <password> <driver-path>')
+        print("Usage: python3 lc_login.py <username> <password> <driver-path>")
         return
-    url = 'https://leetcode.com/accounts/login/'
-    url_problems = 'https://leetcode.com/problemset/all/'
+    url = "https://leetcode.com/accounts/login/"
+    url_progress = "https://leetcode.com/progress/"
     username = sys.argv[1]
     password = sys.argv[2]
     DRIVER_PATH = sys.argv[3]
@@ -49,7 +80,8 @@ def main():
     try:
         driver = get_driver(DRIVER_PATH)
         login(url, username, password, driver)
-        get_progress(url_problems, driver)
+        get_progress(url_progress, driver)
+        sign_out(url_progress, driver)
     finally:
         driver.quit()
 
